c116bd6ba7fec0a27f0e0e5e57f1e3f6da4751a4
AdiMonro3 <adi7ya.t@gmail.com>
2024-04-11 23:32:42 +0530

    variable ,identififer,input

---
+++ basics.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter your first number ";
+    cin>>a;
+    cout<<"enter your second numbr ";
+    cin>>b;
+    c=a;
+    a=b;
+    b=c;
+
+    cout<<"after reversing a is "<<a;
+    cout<<"\nafter reversing b is "<<b;
+} 
